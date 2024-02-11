af9d1739670ccf41327da7ddeafcec829a44ba36
lindsaymcclintock <154388061+lindsaymcclintock@users.noreply.github.com>
2024-02-11 08:36:50 -0600

    geojsonTutorial complete

---
+++ js/geojsonTutorial.js
@@ -0,0 +1,117 @@
+//use the .map class to create map and .setView method to determine 
+//coordinates where map should first appear and at what level of zoom
+var map = L.map('map').setView([45,-90], 5);
+
+//use tileLayer to call tiles from server
+//addTo used to add layer map
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+//create feature and set properties and geometry
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
+//create feature in GeoJSON format and add to map
+L.geoJSON(geojsonFeature).addTo(map);
+
+//create line features given coordinates
+var myLines = [{
+    "type": "LineString",
+    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+}, {
+    "type": "LineString",
+    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+}];
+//style for line featuures
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+//create feature in GeoJSON format and add to map
+L.geoJSON(myLines, {
+    style: myStyle
+}).addTo(map);
+
+//create polygon with bounding coordinates
+var states = [{
+    "type": "Feature",
+    "properties": {"party": "Republican"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-104.05, 48.99],
+            [-97.22,  48.98],
+            [-96.58,  45.94],
+            [-104.03, 45.94],
+            [-104.05, 48.99]
+        ]]
+    }
+}, {
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-109.05, 41.00],
+            [-102.06, 40.99],
+            [-102.03, 36.99],
+            [-109.04, 36.99],
+            [-109.05, 41.00]
+        ]]
+    }
+}];
+//create feature in GeoJSON format and add to map
+//set style for polygons based on property
+L.geoJSON(states, {
+    style: function(feature) {
+        switch (feature.properties.party) {
+            case 'Republican': return {color: "#ff0000"};
+            case 'Democrat':   return {color: "#0000ff"};
+        }
+    }
+}).addTo(map);
+
+//style options for marker
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+
+//create feature in GeoJSON format
+//pointToLayer will create Leaflet layer from GeoJSON point
+//points in layer will be circles from circleMarker method
+L.geoJSON(geojsonFeature, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+}).addTo(map);
+
+//create function that binds popup to features using bindPopup
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+};
+//create feature in GeoJSON format and add to map
+L.geoJSON(geojsonFeature, {
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+
