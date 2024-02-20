81d405bbb4f316fb7134a9aeb3f07fadbddb9990
lindsaymcclintock <154388061+lindsaymcclintock@users.noreply.github.com>
2024-02-19 19:45:52 -0600

    Activity 6

--- js/main.js
+++ js/main.js
@@ -1,57 +1,214 @@
 /* Map of GeoJSON data from airPollution.geojson */
-//declare map var in global scope
-var map = L.map('map').setView([20,0],1.5);
+//GOAL: Proportional symbols representing attribute values of mapped features
+var map;
+var minValue;
 
-    //add OSM base tilelayer
+//Step 1. Create the Leaflet map
+function createMap(){
+
+    //create the map
+    map = L.map('map', {
+        center: [0, 0],
+        zoom: 2
+    });
+//add OSM base tilelayer
     L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-//call getData function
+    getData(map);
+};
 
- //function to attach popups to each mapped feature
-function onEachFeature(feature, layer) {
-    //no property named popupContent; instead, create html string with all properties
-    var popupContent = "";
-    if (feature.properties) {
-        //loop to add feature property names and values to html string
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+function calculateMinValue(data){
+    //create empty array to store all data values
+    var allValues = [];
+    //loop through each city
+    for(var city of data.features){
+        //loop through each year
+        for(var year = 2013; year <= 2019; year+=1){
+              //get population for current year
+              var value = city.properties["PM_"+ String(year)];
+              //add value to array
+              allValues.push(value);
         }
-        layer.bindPopup(popupContent);
-    };
+    }
+    //get minimum value of our array
+    var minValue = Math.min(...allValues)
+
+    return minValue;
 }
-    
-//function to retrieve the data and place it on the map
-    function getData(){
-        //load the data
-        fetch("data/airPollution.geojson")
-            .then(function(response){
-                return response.json();
-            })
-            .then(function(json){
-               //create marker options
-            var geojsonMarkerOptions = {
-                radius: 5,
-                fillColor: "#f80c00",
-                color: "#ffffff",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = 5;
+    //Flannery Apperance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    return radius;
+};
+
+
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng, attributes){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = attributes[0];
+    console.log(attribute);
+    //create marker options
+    var options = {
+        fillColor: "#e70000",
+        color: "#ffffff",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string
+    var year = attribute.split("_")[1];
+    var popupContent = "<i>Annual mean concentration of particulate matter</i>";
+    popupContent += "<p><b>City:</b> " + feature.properties.City + "</p>";
+  
+    popupContent += "<p><b>PM2.5:</b> " + feature.properties[attribute] + " (μg/m3) in "+"<b>" + year + "</b>";
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-options.radius) 
+    });
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//Step 3: Add circle markers for point features to the map
+function createPropSymbols(data, attributes){
+
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        }
+    }).addTo(map);
+};
+
+//Create new sequence controls
+function createSequenceControls(attributes){
+    //create range input element (slider)
+    var slider = "<input class='range-slider' type='range'></input>";
+    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+
+    //set slider attributes
+    document.querySelector(".range-slider").max = 6;
+    document.querySelector(".range-slider").min = 0;
+    document.querySelector(".range-slider").value = 0;
+    document.querySelector(".range-slider").step = 1;
+
+    //add step buttons
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="reverse"></button>');
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward"></button>');
+
+    //replace button content with images
+    document.querySelector('#reverse').insertAdjacentHTML('beforeend',"<img src='img/reverse.png'>")
+    document.querySelector('#forward').insertAdjacentHTML('beforeend',"<img src='img/forward.png'>")
+
+    //click listener for buttons
+    document.querySelectorAll('.step').forEach(function(step){
+        step.addEventListener("click", function(){
+            var index = document.querySelector('.range-slider').value;
+
+            // increment or decrement depending on button clicked
+            if (step.id == 'forward'){
+                index++;
+                //if past the last attribute, wrap around to first attribute
+                index = index > 6 ? 0 : index;
+            } else if (step.id == 'reverse'){
+                index--;
+                //if past the first attribute, wrap around to last attribute
+                index = index < 0 ? 6 : index;
             };
-              //create a Leaflet GeoJSON layer and add it to the map
-              //use pointToLayer to create Leaflet layer from GeoJSON points
-              //circleMarker methond to use circle icons
-              //call onEachFeature function to attach popups
-            L.geoJson(json, {
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                    
-                },  
-                onEachFeature: onEachFeature
-            }).addTo(map); //add points to map
-        });
+
+            //update slider
+            document.querySelector('.range-slider').value = index;
+            updatePropSymbols(attributes[index]);
+        })
+    })
+
+    //Step 5: input listener for slider
+    document.querySelector('.range-slider').addEventListener('input', function(){            
+        //get the new index value
+        var index = this.value;
+        updatePropSymbols(attributes[index]);
+    });
+
+};
+
+//Resize proportional symbols according to new attribute values
+function updatePropSymbols(attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            //access feature properties
+            var props = layer.feature.properties;
+
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+
+            var year = attribute.split("_")[1];
+            var popupContent = "<i>Annual mean concentration of particulate matter</i>";
+            popupContent += "<p><b>City:</b> " + props.City + "</p>";
+  
+            popupContent += "<p><b>PM2.5:</b> " + props[attribute] + " (μg/m3) in "+"<b>" + year + "</b>";
+
+            //update popup content            
+            popup = layer.getPopup();            
+            popup.setContent(popupContent).update();
         };
-                
-                
-getData();//call getData function
+    });
+};
+
+// build an attributes array from the data
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with PM values
+        if (attribute.indexOf("PM") > -1){
+            attributes.push(attribute);
+        };
+    };
+
+    return attributes;
+};
+
+
+// Import GeoJSON data
+function getData(){
+    //load the data
+    fetch("data/airPollution.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //create attribute array
+            var attributes = processData(json);
+            //calculate minimum data value
+            minValue = calculateMinValue(json);
+            //call function to create proportional symbols
+            createPropSymbols(json, attributes);
+            //call function to create controls
+            createSequenceControls(attributes);
+        })
+};
+
+document.addEventListener('DOMContentLoaded',createMap)
